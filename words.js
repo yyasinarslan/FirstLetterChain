a5170d226c72927c7a17a1b307ba37372c6dcdfb
yasinarslan <yasinarslan15@outlook.com>
2026-01-24 14:56:21 +0300

    Separate word list into another js file

---
+++ words.js
@@ -0,0 +1,14 @@
+const computerLists = [
+    ["Telefon", "Şarjı", "Aleti", "Çantası", "Askısı", "İpi", "Kopuk"],
+    ["Kahve", "Fincanı", "Tabağı", "Kenarı", "Kırık", "Cam", "Parçası"],
+    ["Okul", "Çantası", "Fermuarı", "Bozuk", "Para", "Üstü", "Kalsın"],
+    ["Yazılım", "Dili", "Yapısı", "Karmaşık", "Sayılar", "Teorisi", "Kitabı"],
+    ["Yağmur", "Damlası", "Çikolata", "Şelalesi", "Suyu", "Şişesi", "Buruşması"],
+    ["Yaz", "Tatili", "Köyü", "Kahvesi", "Falı", "Bakmak", "Görmek"],
+    ["Hamburger", "Ekmeği", "Parası", "Kasası", "Şifresi", "Kırmak", "Dökmek"],
+    ["Deniz", "Sörfü", "Tahtası", "Kurusu", "Fasulye", "Fiyatı", "Etiketi"],
+    ["Kalp", "Krizi", "Masası", "Ayağı", "Parmağı", "Çıtlaması", "Kırıldı"],
+    ["Uğur", "Böceği", "İlacı", "Kutusu", "Oyunu", "Konsolu", "Aynası"],
+    ["Kısa", "Mesafe", "Koşusu", "Bandı", "Genişliği", "Ayarı"], // 6 Kelimelik Örnek
+    ["Elma", "Şekeri", "Pancarı", "Motoru", "Yağı", "Lekesi"]    // 6 Kelimelik Örnek
+];
